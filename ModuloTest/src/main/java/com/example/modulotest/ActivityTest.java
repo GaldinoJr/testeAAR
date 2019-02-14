27f33b930560c94a78f023d366dd6595ef0b123a
galdinojr <galdinodbz@gmail.com>
2019-02-14 18:41:52 -0200

    teste de layout

---
+++ ModuloTest/src/main/java/com/example/modulotest/ActivityTest.java
@@ -0,0 +1,13 @@
+package com.example.modulotest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
